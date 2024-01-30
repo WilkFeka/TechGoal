ad75fa8fd46803a59ca1d882aa69da2e8f76409f
Mauri <maurijunk02@gmail.com>
2024-01-30 19:06:52 -0300

    modificacion de biblioteca de clases

--- CapaDatos/Class1.cs
+++ CapaDatos/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace CapaDatos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
 {
     public class Class1
     {
-
     }
 }
