da24a5af04fe2795def3a9b862586941ffdb5420
Mauri <maurijunk02@gmail.com>
2024-01-31 01:38:08 -0300

    Se crearon cambios en presentacion login, falta terminar

--- CapaPresentacion/Login.Designer.cs
+++ CapaPresentacion/Login.Designer.cs
@@ -28,98 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblEmail = new System.Windows.Forms.Label();
-            this.lblClave = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.imgLogoText = new System.Windows.Forms.PictureBox();
             this.txtClave = new System.Windows.Forms.TextBox();
-            this.btnCancelar = new FontAwesome.Sharp.IconButton();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.btnIngresar = new FontAwesome.Sharp.IconButton();
+            this.btnCancelar = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogoText)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblEmail
+            // imgLogoText
             // 
-            this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(151, 65);
-            this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(93, 13);
-            this.lblEmail.TabIndex = 0;
-            this.lblEmail.Text = "Correo electronico";
+            this.imgLogoText.BackColor = System.Drawing.Color.Transparent;
+            this.imgLogoText.Image = global::CapaPresentacion.Properties.Resources.LogoText;
+            this.imgLogoText.Location = new System.Drawing.Point(3, 3);
+            this.imgLogoText.Name = "imgLogoText";
+            this.imgLogoText.Size = new System.Drawing.Size(447, 443);
+            this.imgLogoText.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgLogoText.TabIndex = 6;
+            this.imgLogoText.TabStop = false;
+            // 
+            // txtClave
+            // 
+            this.txtClave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(35)))), ((int)(((byte)(64)))));
+            this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtClave.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.Location = new System.Drawing.Point(3, 177);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '*';
+            this.txtClave.Size = new System.Drawing.Size(477, 46);
+            this.txtClave.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(35)))), ((int)(((byte)(64)))));
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(3, 59);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 10, 3, 20);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(477, 46);
+            this.txtEmail.TabIndex = 1;
             // 
             // lblClave
             // 
             this.lblClave.AutoSize = true;
-            this.lblClave.Location = new System.Drawing.Point(151, 131);
+            this.lblClave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblClave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblClave.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblClave.Location = new System.Drawing.Point(3, 138);
+            this.lblClave.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
             this.lblClave.Name = "lblClave";
-            this.lblClave.Size = new System.Drawing.Size(61, 13);
+            this.lblClave.Size = new System.Drawing.Size(477, 29);
             this.lblClave.TabIndex = 1;
             this.lblClave.Text = "Contraseña";
             // 
-            // txtEmail
+            // lblEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(154, 81);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(207, 20);
-            this.txtEmail.TabIndex = 2;
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblEmail.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblEmail.Location = new System.Drawing.Point(3, 20);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(477, 29);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Correo electronico";
             // 
-            // txtClave
+            // flowLayoutPanel1
             // 
-            this.txtClave.Location = new System.Drawing.Point(154, 163);
-            this.txtClave.Name = "txtClave";
-            this.txtClave.PasswordChar = '*';
-            this.txtClave.Size = new System.Drawing.Size(207, 20);
-            this.txtClave.TabIndex = 3;
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1084, 461);
+            this.flowLayoutPanel1.TabIndex = 7;
             // 
-            // btnCancelar
+            // flowLayoutPanel2
             // 
-            this.btnCancelar.IconChar = FontAwesome.Sharp.IconChar.None;
-            this.btnCancelar.IconColor = System.Drawing.Color.Black;
-            this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnCancelar.Location = new System.Drawing.Point(286, 237);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
-            this.btnCancelar.TabIndex = 4;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            this.flowLayoutPanel2.Controls.Add(this.imgLogoText);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(450, 446);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel3.Controls.Add(this.lblEmail);
+            this.flowLayoutPanel3.Controls.Add(this.txtEmail);
+            this.flowLayoutPanel3.Controls.Add(this.lblClave);
+            this.flowLayoutPanel3.Controls.Add(this.txtClave);
+            this.flowLayoutPanel3.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.SetFlowBreak(this.flowLayoutPanel3, true);
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(506, 47);
+            this.flowLayoutPanel3.Margin = new System.Windows.Forms.Padding(50, 3, 3, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(485, 357);
+            this.flowLayoutPanel3.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnIngresar);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Location = new System.Drawing.Point(3, 222);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 102);
+            this.panel1.TabIndex = 7;
             // 
             // btnIngresar
             // 
             this.btnIngresar.IconChar = FontAwesome.Sharp.IconChar.None;
             this.btnIngresar.IconColor = System.Drawing.Color.Black;
             this.btnIngresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnIngresar.Location = new System.Drawing.Point(154, 237);
+            this.btnIngresar.Location = new System.Drawing.Point(3, 41);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
-            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Size = new System.Drawing.Size(182, 50);
+            this.btnIngresar.TabIndex = 3;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
             this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnCancelar.IconColor = System.Drawing.Color.Black;
+            this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCancelar.Location = new System.Drawing.Point(302, 41);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(172, 50);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnIngresar);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.txtClave);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.lblClave);
-            this.Controls.Add(this.lblEmail);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(19)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1084, 461);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(1100, 500);
             this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogoText)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.PictureBox imgLogoText;
         private System.Windows.Forms.Label lblClave;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtClave;
-        private FontAwesome.Sharp.IconButton btnCancelar;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Panel panel1;
         private FontAwesome.Sharp.IconButton btnIngresar;
+        private FontAwesome.Sharp.IconButton btnCancelar;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
